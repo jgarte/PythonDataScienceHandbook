c5a163bff94d34cc8bd4e48e6f619e2c36b6ce3c
jgart <47760695+jgarte@users.noreply.github.com>
2021-09-28 22:18:04 -0400

    Add guix manifest for binderlite

---
+++ requirements.scm
@@ -0,0 +1,16 @@
+;; these need to be packaged for guix
+;; line_profiler
+;; memory_profiler
+;; pandas-datareader
+
+(specifications->manifest
+ '("python-matplotlib" 
+    "python-numpy"
+    "python-pandas"
+    "python-scipy"
+    "python-scikit-learn"
+    "python-scikit-image"
+    "python-pillow"
+    "python-seaborn"
+    "python-numexpr"
+    "python-netcdf4"))
